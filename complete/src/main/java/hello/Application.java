e2f96077b31575c7637a2a77149a4c48164cf7d5
Srinivas Kuchimanchi <ksrinivas@newtglobal.chn.local>
2017-03-01 15:22:36 +0530

    complete repo commit

---
+++ complete/src/main/java/hello/Application.java
@@ -0,0 +1,27 @@
+package hello;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+    @RequestMapping("/")
+    public String home() throws Exception {
+    	return "Hello Docker World.....Server Host:"+request.getLocalAddr()+"...Updated";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
