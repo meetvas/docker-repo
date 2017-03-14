853f1008c0c0309e28d754e159cf4b5e9197973b
ksrinivas <ksrinivas@Newt-Css16.newtglobal.chn.local>
2017-03-14 13:27:44 +0530

    log updates

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,10 +1,9 @@
 package hello;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,19 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+	public final Logger log =  LoggerFactory.getLogger(this.getClass());
+	
 	@Autowired
 	private HttpServletRequest request;
 	
     @RequestMapping("/")
     public String home() throws Exception {
-    	try {
-    		System.out.println("jdbc:mysql://192.168.3.20:3306/mysql-galera");	
+    	/*try {
+    		System.out.println("jdbc:mysql://192.168.3.20:3306/mysql");	
     	Class.forName("com.mysql.jdbc.Driver");
     	Connection con=DriverManager.getConnection(	"jdbc:mysql://192.168.3.20:3306/mysql-galera","root","mypassword");
     	con.close();
     	} catch(Exception e) {
     		System.out.println(e);
-    	}
+    	}*/
+    	
+    	log.debug("debug message");
+    	log.warn("warn message");
+    	log.info("info message");
+    	log.error("error message");
+    	
     	return "Hello Docker World.....Server Host:"+request.getLocalAddr()+"...Final Update for port is the final one";
     }
 
