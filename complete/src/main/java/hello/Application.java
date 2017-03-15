ea5ec315d29aff051f1c0dc0e5158170537e5a03
ksrinivas <ksrinivas@Newt-Css16.newtglobal.chn.local>
2017-03-15 19:20:57 +0530

    log updates

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,6 +3,7 @@ package hello;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
